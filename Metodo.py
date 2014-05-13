bbde7c4440e3b4136af6bc5563d5e1d783922974
Gustavo Lima de Magalhães <Glmagalhaes@hotmail.com>
2014-05-12 22:05:39 -0300

    Bisseção
    Método da bisseção implementado junto com funções auxiliares.

--- Metodo.py
+++ Metodo.py
@@ -16,3 +16,40 @@ class MetodoExemplo:
 	def executaMetodo(self):
 		#Executa
 		'Executed'
+		
+#Methods codes
+#euler constant
+euler = 2.71828
+
+#return t in function of f
+def f(t):
+    return 70*pow(euler,(-1.5*t))+2.5*pow(euler,(-0.075*t))-9
+
+#return the numbers signal
+def signal(x):
+    if x<0:
+        return -1
+    else:
+        return +1
+
+def bissecao():
+
+    iterations = 1
+    MAX_iterations = 20
+    MIN_error = 0.00001
+
+    a = 0.0
+    b = 3.0
+    c = 0.0
+    print "Iteration\ta\t\tb\t\tc\t\tf(c)"
+    while 1: # iterations ≤ MAX_iterations: # limit iterations to prevent infinite loop
+        c = (a + b)/2 # new midpoint
+        print iterations,"\t\t",a,"\t",b,"\t",c,"\t",f(c)
+        if f(c) == 0 or ((b - a)/2) < MIN_error: # solution found
+            break;
+        iterations+=1 # increment step counter
+        if signal(f(c)) == signal(f(a)):  # create  new interval
+            a = c
+        else:
+            b = c
+
